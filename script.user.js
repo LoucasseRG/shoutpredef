b72c408350e17c4148e0b381f473573d03009030
Loucasse <takazyt@gmail.com>
2015-11-01 17:15:40 +0100

    Create script.user.js

---
+++ script.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         Shout predef
+// @namespace    https://realitygaming.fr/
+// @include      https://realitygaming.fr/
+// @include      https://realitygaming.fr/chatbox
+// @version      1
+// @description  Ce script ajoute des réponses prédéfinis pour la shoutbox
+// @author       Loucasse
+// @match        https://realitygaming.fr/
+// @grant        none
+// ==/UserScript==
+// SCRIPT CODE PAR LOUCASSE
+$(document).ready(function(){
+    $.getScript('https://rawgit.com/LoucasseRG/shoutpredef/master/script.js');
+});
